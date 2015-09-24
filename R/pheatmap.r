ac19e17ab15c6de059f772a4d663d8eee712b963
Li meng <mengli122@icloud.com>
2015-09-24 16:24:10 -0400

    Update pheatmap.r

--- R/pheatmap.r
+++ R/pheatmap.r
@@ -222,7 +222,7 @@ draw_colnames = function(coln, gaps, ...){
     coord = find_coordinates(length(coln), gaps)
     x = coord$coord - 0.5 * coord$size
     
-    res = textGrob(coln, x = x, y = unit(1, "npc") - unit(3, "bigpts"), vjust = 0.5, hjust = 0, rot = 270, gp = gpar(...))
+    res = textGrob(coln, x = x, y = unit(1, "npc") - unit(3, "bigpts"), vjust = 0.5, hjust = 0, rot = 0, gp = gpar(...))
     
     return(res)
 }
